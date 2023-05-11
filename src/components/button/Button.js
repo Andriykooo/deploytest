d240dfea406910a6a0f3f82b7bad1c099c2d2e1b
AndriiBoichuk <boichukandri9@gmail.com>
2023-05-11 14:53:08 +0300

    test

--- src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Button = ({ className, text, onClick, disabled }) => {
-  return (
-    <button className={className} onClick={onClick} disabled={disabled}>
-      {text}
-    </button>
-  );
-};
