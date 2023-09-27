e8d2455a67dc06e66141575a313b4eccb2423a99
Andriykooo <boichukandri9@gmail.com>
2023-09-27 15:54:55 +0300

    qwewqe

--- src/app/i18n/client.js
+++ src/app/i18n/client.js
@@ -1,36 +1,38 @@
-import { useEffect } from "react";
-import i18next from "i18next";
-import {
-  initReactI18next,
-  useTranslation as useTranslationOrg,
-} from "react-i18next";
-import resourcesToBackend from "i18next-resources-to-backend";
-import { getOptions, languages } from "./settings";
-import { useClientPathname } from "@/hooks/useClientPathname";
+// import { useEffect } from "react";
+// import i18next from "i18next";
+// import {
+//   initReactI18next,
+//   useTranslation as useTranslationOrg,
+// } from "react-i18next";
+// import resourcesToBackend from "i18next-resources-to-backend";
+// import { getOptions, languages } from "./settings";
+// import { useClientPathname } from "@/hooks/useClientPathname";
 
-const runsOnServerSide = typeof window === "undefined";
+// const runsOnServerSide = typeof window === "undefined";
 
-i18next
-  .use(initReactI18next)
-  .use(
-    resourcesToBackend((language, namespace) =>
-      import(`./locales/${language}.json`).then((res) => res[namespace])
-    )
-  )
-  .init({
-    ...getOptions(),
-    preload: runsOnServerSide ? languages : [],
-  });
+// i18next
+//   .use(initReactI18next)
+//   .use(
+//     resourcesToBackend((language, namespace) =>
+//       import(`./locales/${language}.json`).then((res) => res[namespace])
+//     )
+//   )
+//   .init({
+//     ...getOptions(),
+//     preload: runsOnServerSide ? languages : [],
+//   });
 
 export function useClientTranslation(ns, options) {
-  const { locale: lng } = useClientPathname();
-  const { t, i18n } = useTranslationOrg(ns, options);
+  // const { locale: lng } = useClientPathname();
+  // const { t, i18n } = useTranslationOrg(ns, options);
 
-  useEffect(() => {
-    if (lng !== i18n.language) {
-      i18n.changeLanguage(lng);
-    }
-  }, [lng, i18n]);
+  // useEffect(() => {
+  //   if (lng !== i18n.language) {
+  //     i18n.changeLanguage(lng);
+  //   }
+  // }, [lng, i18n]);
 
-  return { t, i18n };
+  const t = () => "qweqweqwe";
+
+  return { t };
 }
--- src/app/i18n/settings.js
+++ src/app/i18n/settings.js
@@ -2,3 +2,9 @@ export const fallbackLng = "en";
 export const languages = [fallbackLng, "de"];
 export const defaultNS = "common";
 export const cookieName = "language";
+
+export function getOptions() {
+  return {
+    lng: "en",
+  };
+}
