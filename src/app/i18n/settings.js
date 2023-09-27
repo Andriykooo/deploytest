8a0f778e424869a024f40f77abb7c5800139ea36
Andriykooo <boichukandri9@gmail.com>
2023-09-27 14:09:37 +0300

    test3

--- src/app/i18n/settings.js
+++ src/app/i18n/settings.js
@@ -1,15 +1,13 @@
-export const fallbackLng = 'en'
-export const languages = [fallbackLng, 'de']
-export const defaultNS = 'common'
-export const cookieName = 'language'
+export const fallbackLng = "en";
+export const languages = [fallbackLng, "de"];
+export const defaultNS = "common";
+export const cookieName = "language";
 
-export function getOptions (lng = fallbackLng, ns = [defaultNS]) {
+export function getOptions() {
   return {
     supportedLngs: languages,
-    fallbackLng,
-    lng,
     fallbackNS: defaultNS,
     defaultNS,
-    ns
-  }
+    ns: [defaultNS],
+  };
 }
