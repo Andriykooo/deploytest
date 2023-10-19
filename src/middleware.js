cc1276fdcd14ec9ac691f8b26057f19591dd4188
Andriykooo <boichukandri9@gmail.com>
2023-10-19 18:37:04 +0300

    reworked middleware 2

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,15 @@
 import { languages } from "./app/i18n/settings";
 
 export const config = {
-  // matcher: '/:lng*'
   matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"],
 };
 
 export function middleware(request) {
-  // const locale = "en";
-  // const { pathname } = request.nextUrl;
+  const locale = "en";
+  const { pathname } = request.nextUrl;
 
-  // if (
-  //   !languages.some((lang) => pathname.startsWith(`/${lang}`)) &&
-  //   !pathname.startsWith("/_next")
-  // ) {
+  // console.log(languages, pathname, !languages.some((lang) => pathname.startsWith(`/${lang}`)));
+  // if (!languages.some((lang) => pathname.startsWith(`/${lang}`))) {
   //   request.nextUrl.pathname = `/${locale}${pathname}`;
   //   return Response.redirect(request.nextUrl);
   // }
