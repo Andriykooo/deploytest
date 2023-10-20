c05d6a8381cfe190263be8fc910048fe04bbd062
Andriykooo <boichukandri9@gmail.com>
2023-10-20 11:38:14 +0300

    t2

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { fallbackLng } from "./app/i18n/settings";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
@@ -7,6 +8,6 @@ export const config = {
 export function middleware(request) {
   console.log(request.nextUrl.pathname);
   if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/en", request.url));
+    return NextResponse.rewrite(new URL(`/${fallbackLng}`, request.url));
   }
 }
