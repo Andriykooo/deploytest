8308af88b95f4b75144947fe7fea5c0286b71de9
Andriykooo <boichukandri9@gmail.com>
2023-10-23 15:49:24 +0300

    test2

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,27 @@
-import { languages } from "./app/i18n/settings";
+// import { languages } from "./app/i18n/settings";
 
-export function middleware(request) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl;
-  const pathnameHasLocale = languages.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
+// export function middleware(request) {
+//   // Check if there is any supported locale in the pathname
+//   const { pathname } = request.nextUrl;
+//   const pathnameHasLocale = languages.some(
+//     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+//   );
 
-  if (pathnameHasLocale) return;
+//   if (pathnameHasLocale) return;
 
-  // Redirect if there is no locale
-  const locale = "en";
-  request.nextUrl.pathname = `/${locale}${pathname}`;
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
-  return Response.redirect(request.nextUrl);
-}
+//   // Redirect if there is no locale
+//   const locale = "en";
+//   request.nextUrl.pathname = `/${locale}${pathname}`;
+//   // e.g. incoming request is /products
+//   // The new URL is now /en-US/products
+//   return Response.redirect(request.nextUrl);
+// }
 
-export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    "/((?!_next).*)",
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Skip all internal paths (_next)
+//     "/((?!_next).*)",
+//     // Optional: only run on root (/) URL
+//     // '/'
+//   ],
+// };
